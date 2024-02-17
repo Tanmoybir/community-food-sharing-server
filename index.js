abe32513dd13dfea342dd00cc1790144e32005c1
Tanmoy Bir <tanmoy7468@gmail.com>
2024-02-17 16:57:25 +0600

    post method apply and insert one data sucessfully

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000
@@ -33,7 +33,18 @@ async function run() {
         res.send(result)
     })
 
-   
+   app.get('/api/v1/foods/:id', async(req,res) => {
+    const id = req.params.id
+    const query = {_id: new ObjectId(id)}
+    const result = await foodCollection.findOne(query)
+    res.send(result)
+   })
+
+   app.post('/api/v1/foods', async(req,res) => {
+    const food = req.body
+    const result = await foodCollection.insertOne(food)
+    res.send(result)
+   })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
