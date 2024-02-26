0fcffe9557ddcf652ceb2f8bc42bdd9bd7c6d57b
Tanmoy Bir <tanmoy7468@gmail.com>
2024-02-26 18:56:26 +0600

    removing node_modules

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser')
 const app = express()
 const port = process.env.PORT || 5000
 
 // meddleWare
-app.use(cors())
+app.use(cors({
+    origin:'http://localhost:5173',
+    credentials:true
+}))
 app.use(express.json())
+app.use(cookieParser())
 
 const secret = process.env.TOKEN_SECRET
 
@@ -28,15 +33,31 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
+        const verifyToken = (req, res, next) => {
+            const {token} = req.cookies
+            if(!token) {
+                return res.status(401).send({message:'Forbidden Access'})
+            }
+            jwt.verify(token,secret, (err,decoded) =>{
+                if(err) {
+                    return res.status(403).send({message:'Forbidden Access'})
+                }
+                req.user = decoded
+                next()
+            })
+        }
+
         const foodCollection = client.db('foodDB').collection('foods')
 
-        app.get('/api/v1/foods', async (req, res) => {
+        app.get('/api/v1/foods', verifyToken, async (req, res) => {
+            const tokenEmail = req.user
+            console.log(tokenEmail);
             const cursor = foodCollection.find()
             const result = await cursor.toArray()
             res.send(result)
         })
 
-        app.get('/api/v1/foods/:id', async (req, res) => {
+        app.get('/api/v1/foods/:id', verifyToken, async (req, res) => {
             const id = req.params.id
             const query = { _id: new ObjectId(id) }
             const result = await foodCollection.findOne(query)
@@ -51,13 +72,14 @@ async function run() {
 
         app.post('/api/v1/user/access-token', async (req, res) => {
             const userEmail = req.body.email
-            const token = jwt.sign(userEmail, secret) 
-            console.log(token);
-            res.cookie('jwt',token,{
-                httpOnly:true,
-                secure:true,
-                sameSite:false
-            }).send({success:true})
+            console.log(userEmail);
+            const token = jwt.sign(userEmail, secret)
+            // console.log(token);
+            res.cookie('token',token, {
+                httpOnly: true,
+                secure: true,
+                sameSite: 'none'
+            }).send({ success: true })
         })
 
         // Send a ping to confirm a successful connection
